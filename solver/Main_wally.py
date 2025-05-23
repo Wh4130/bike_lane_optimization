8e384933e568c5d70793adb9558f95a8f6f67899
JoriFS <schmidtjorigmail.com>
2025-05-23 11:21:28 +0800

    minor changes

--- solver/Main_wally.py
+++ solver/Main_wally.py
@@ -180,7 +180,7 @@ class Model:
         # at most two reads connected to each intersection:
         for i in Intersections['road_i'].unique():
             # find all road_j’s paired with this i
-            self.model.addConstr(gp.quicksum(self.y[i, j] for j in Intersections.loc[Intersections['road_i'] == i, 'road_j']) <= 2)
+            self.model.addConstr(gp.quicksum(self.y[i, j] for j in Intersections.loc[Intersections['road_i'] == i, 'road_j']) <= 1)
 
 
         # area coverage constraint
